c5667f1d6cc12062e003b38f874d879353cd6851
airi <airi.hisa@icloud.com>
2026-04-20 22:08:06 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,67 +6,18 @@ import Home from './App/Home'
 import List from './App/List'
 import AboutUs from './App/AboutUs'
 import Category from './App/Category'
-import Images from './App/Images'
+// import Images from './App/Images' // ← 1. この行を削除
 
 import Tabbar from './App/Tabbar'
 import config from "./config.json";
 import Papa from 'papaparse'
 
-// You can see config.json after running `npm start` or `npm run build`
-// import config from './config.json'
-
-const sortShopList = async (shopList: Pwamap.ShopData[]) => {
-
-  // 新着順にソート
-  return shopList.sort(function (item1, item2) {
-    return Date.parse(item2['タイムスタンプ']) - Date.parse(item1['タイムスタンプ'])
-  });
-
-}
+// ... (sortShopList の定義はそのまま)
 
 const App = () => {
   const [shopList, setShopList] = React.useState<Pwamap.ShopData[]>([])
 
-  React.useEffect(() => {
-    fetch(config.data_url)
-    .then((response) => {
-      return response.ok ? response.text() : Promise.reject(response.status);
-    })
-    .then((data) => {
-
-      Papa.parse(data, {
-        header: true,
-        complete: (results) => {
-          const features = results.data
-
-          const nextShopList: Pwamap.ShopData[] = []
-          for (let i = 0; i < features.length; i++) {
-            const feature = features[i] as Pwamap.ShopData
-            if (!feature['緯度'] || !feature['経度'] || !feature['スポット名']) {
-              continue;
-            }
-            if (!feature['緯度'].match(/^[0-9]+(\.[0-9]+)?$/)) {
-              continue
-            }
-            if (!feature['経度'].match(/^[0-9]+(\.[0-9]+)?$/)) {
-              continue
-            }
-            const shop = {
-              // @ts-ignore
-              index: i,
-              ...feature
-            }
-
-            nextShopList.push(shop)
-          }
-          sortShopList(nextShopList).then((sortedShopList) => {
-            setShopList(sortedShopList)
-          })
-        }
-      });
-    });
-  }, [])
-
+  // ... (useEffect の fetch 処理はそのまま)
 
   return (
     <div className="app">
@@ -75,7 +26,8 @@ const App = () => {
           <Route path="/" element={<Home data={shopList} />} />
           <Route path="/list" element={<List data={shopList} />} />
           <Route path="/category" element={<Category data={shopList} />} />
-          <Route path="/images" element={<Images data={shopList} />} />
+          {/* 2. 下の Route 行を削除 */}
+          {/* <Route path="/images" element={<Images data={shopList} />} /> */}
           <Route path="/about" element={<AboutUs />} />
         </Routes>
       </div>
