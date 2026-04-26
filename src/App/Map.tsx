8ecf8f505eb8a409bfad13190c1c65f5dada7e43
airi <airi.hisa@icloud.com>
2026-04-26 18:06:14 +0900

    Update Map.tsx

--- src/App/Map.tsx
+++ src/App/Map.tsx
@@ -192,7 +192,7 @@ React.useEffect(() => {
 
     const map = new geolonia.Map({
       container: mapNode.current,
-      style: 'geolonia/mono',
+      style: 'geolonia/notebook',
     });
 
     const onMapLoad = () => {
